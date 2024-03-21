17776cde83dafda2cbf6a3788048eaca6426ba42
soheil <soheilh105@gmail.com>
2024-03-22 01:21:03 +0330

    Install django-ckeditor

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product.apps.ProductConfig',
     'user.apps.UserConfig',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
