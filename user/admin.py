c78b7beb300b9c49e8b9472ec2f111e297b338db
soheil <soheilh105@gmail.com>
2024-03-22 03:53:00 +0330

    feat: Add AuthorUser model to admin interface
    - Created AuthorUserAdmin class to customize the display and behavior of AuthorUser model in the admin interface.
    - Added list display, search fields, list filters, and fieldsets for improved usability and management.
    - Configured fieldsets to organize account and personal information.
    - Included add_fieldsets to define fields required for adding new AuthorUser instances.
    - Set is_staff field as readonly to prevent modification in the admin interface.

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import AdminUser, CustomerUser
+from .models import AdminUser, CustomerUser, AuthorUser
 
 # Register AdminUser model
 class AdminUserAdmin(UserAdmin):
@@ -42,3 +42,26 @@ class CustomerUserAdmin(UserAdmin):
         super(CustomerUserAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(CustomerUser, CustomerUserAdmin)
+
+
+# Register AuthorUser model
+class AuthorUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'name', 'lastname', "bio", "telegram", "instagram", "twitter", "facebook")
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        ('Account Information', {
+            'fields': ('username', 'password', )
+        }),
+        ('Personal Information', {
+            'fields': ('first_name', 'last_name', 'email', 'bio', 'telegram', 'instagram', 'twitter', 'facebook')
+        }),
+    )
+    add_fieldsets = (
+        ('Account Information', {
+            'fields': ('phone', 'password1', 'password2',)
+        }),
+    )
+    readonly_fields = ('is_staff',)
+
+admin.site.register(AuthorUser, AuthorUserAdmin)
