bef775ca6f030f14975d1ed1a13a42b6037b348f
soheil <soheilh105@gmail.com>
2024-03-23 14:31:43 +0330

    feat: Add Post and Category models to admin interface
    - Created PostAdmin and CategoryAdmin classes to customize the display and behavior of Post and Category models in the admin interface.
    - Added list display, filters, search fields, and prepopulated fields for better usability and management.
    - Implemented 'category_to_str' method in PostAdmin to display associated categories as a list.
    - Registered Post, Category, and Comment models with their respective admin classes.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post, Category, Comment
 
-# Register your models here.
+# Register Post Model
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'slug', 'status', 'category_to_str',)
+	list_filter = ('publish', 'status',)
+	search_fields = ('title', 'description', 'content',)
+	prepopulated_fields = {'slug': ('title',)}
+	ordering = ('-status', '-publish',)
+	# exclude = ('author', )
+
+	def category_to_str(self, obj):
+		return [category for category in obj.category.all()]
+	category_to_str.short_description = 'Categories'
+
+admin.site.register(Post, PostAdmin)
+
+# Register Category Model
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug', 'status',)
+	list_filter = ('status',)
+	search_fields = ('name')
+	prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment)
