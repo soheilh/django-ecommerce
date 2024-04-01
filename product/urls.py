dcdb715489e6aa70f10d3e07f10961a4dd078eac
soheil <soheilh105@gmail.com>
2024-04-01 20:28:06 +0330

    refactor: Update URL routing for product-specific comments

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
-router.register('(?P<id>\d+)/comments', views.CommentViewSet, basename='comments')
+router.register('products/(?P<id>\d+)/comments', views.CommentViewSet, basename='comments')
 router.register('categories', views.CategoryViewSet, basename='categories')
 router.register('brands', views.BrandViewSet, basename='brands')
 router.register('colors', views.ColorViewSet, basename='colors')
